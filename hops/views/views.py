27533b29805be7cd6d725566cfb168a923d794bd
Tomas James <tomas.james@me.com>
2018-05-30 10:33:00 +0100

    Adds a basic landing page for the user to land on. Need to incorporate model specific selections here.

--- hops/views/views.py
+++ hops/views/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('hops/hops.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
