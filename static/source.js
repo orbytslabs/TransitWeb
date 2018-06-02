a2fe04648abf3ce4eefebfc55b25b3ec93d8141d
Tomas James <tomas.james@me.com>
2018-06-02 18:57:00 +0100

    Plots a circle on the click location

--- static/source.js
+++ static/source.js
@@ -20,3 +20,39 @@ $(document).ready(function (e) {
         });
     });
 });
+
+$(document).ready(function (e) {
+    var canvas = document.getElementById("finderImageCanvas");
+    var context = canvas.getContext('2d');
+
+    function createImageOnCanvas(imageId) {
+        canvas.style.display = "block";
+        document.getElementById("images").style.overflowY = "hidden";
+        var img = new Image(300, 300);
+        img.src = document.getElementById(imageId).src;
+        context.drawImage(img, (0), (0)); //onload....
+    }
+
+    function draw(e) {
+        var pos = getMousePos(canvas, e);
+        posx = pos.x;
+        posy = pos.y;
+        context.fillStyle = "transparent";
+        context.strokeStyle = "#000000";
+        context.lineWidth = 2;
+        context.beginPath();
+        context.arc(posx, posy, 8, 0, 2*Math.PI);
+        context.fill();
+        context.stroke();
+    }
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+    }
+
+    window.draw = draw;
+});
